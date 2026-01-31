94a557577be9a19337571cc0a75833f9298543fa
spotdemo4 <me@trev.xyz>
2026-01-31 14:58:03 -0500

    init

---
+++ .github/README.md
@@ -0,0 +1,44 @@
+# template
+
+[![check](https://img.shields.io/github/actions/workflow/status/spotdemo4/go-template/check.yaml?branch=main&logo=github&logoColor=%23bac2de&label=check&labelColor=%23313244)](https://github.com/spotdemo4/go-template/actions/workflows/check.yaml/)
+[![vulnerable](https://img.shields.io/github/actions/workflow/status/spotdemo4/go-template/vulnerable.yaml?branch=main&logo=github&logoColor=%23bac2de&label=vulnerable&labelColor=%23313244)](https://github.com/spotdemo4/go-template/actions/workflows/vulnerable.yaml)
+
+default template for projects
+
+part of [spotdemo4/templates](https://github.com/spotdemo4/templates)
+
+## requirements
+
+- [nix](https://nixos.org/)
+- [direnv](https://direnv.net/) (optional)
+
+## getting started
+
+initialize direnv:
+
+```elm
+ln -s .envrc.project .envrc &&
+direnv allow
+```
+
+or manually enter the development environment:
+
+```elm
+nix develop
+```
+
+### check
+
+```elm
+nix flake check
+```
+
+### release
+
+releases are automatically created for [significant](https://www.conventionalcommits.org/en/v1.0.0/#summary) changes
+
+to manually create a version bump:
+
+```elm
+bumper
+```
---
+++ flake.nix
@@ -0,0 +1,149 @@
+{
+  description = "template";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.trev.zip/nur"
+    ];
+    extra-trusted-public-keys = [
+      "nur:70xGHUW1+1b8FqBchldaunN//pZNVo6FKuPL4U/n844="
+    ];
+  };
+
+  inputs = {
+    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    trev = {
+      url = "github:spotdemo4/nur";
+      inputs.systems.follows = "systems";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      trev,
+      ...
+    }:
+    trev.libs.mkFlake (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            trev.overlays.packages
+            trev.overlays.libs
+          ];
+        };
+        fs = pkgs.lib.fileset;
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            name = "dev";
+            shellHook = pkgs.shellhook.ref;
+            packages = with pkgs; [
+              # formatters
+              nixfmt
+              prettier
+
+              # util
+              bumper
+              flake-release
+            ];
+          };
+
+          bump = pkgs.mkShell {
+            name = "bump";
+            packages = with pkgs; [
+              bumper
+            ];
+          };
+
+          release = pkgs.mkShell {
+            name = "release";
+            packages = with pkgs; [
+              flake-release
+            ];
+          };
+
+          update = pkgs.mkShell {
+            name = "update";
+            packages = with pkgs; [
+              renovate
+            ];
+          };
+
+          vulnerable = pkgs.mkShell {
+            name = "vulnerable";
+            packages = with pkgs; [
+              # nix
+              flake-checker
+
+              # actions
+              octoscan
+            ];
+          };
+        };
+
+        checks = pkgs.lib.mkChecks {
+          actions = {
+            src = fs.toSource {
+              root = ./.;
+              fileset = ./.github/workflows;
+            };
+            deps = with pkgs; [
+              action-validator
+              octoscan
+            ];
+            script = ''
+              action-validator **/*.yaml
+              octoscan scan .
+            '';
+          };
+
+          renovate = {
+            src = fs.toSource {
+              root = ./.github;
+              fileset = ./.github/renovate.json;
+            };
+            deps = with pkgs; [
+              renovate
+            ];
+            script = ''
+              renovate-config-validator renovate.json
+            '';
+          };
+
+          nix = {
+            src = fs.toSource {
+              root = ./.;
+              fileset = fs.fileFilter (file: file.hasExt "nix") ./.;
+            };
+            deps = with pkgs; [
+              nixfmt-tree
+            ];
+            script = ''
+              treefmt --ci
+            '';
+          };
+
+          prettier = {
+            src = fs.toSource {
+              root = ./.;
+              fileset = fs.fileFilter (file: file.hasExt "yaml" || file.hasExt "json" || file.hasExt "md") ./.;
+            };
+            deps = with pkgs; [
+              prettier
+            ];
+            script = ''
+              prettier --check .
+            '';
+          };
+        };
+
+        formatter = pkgs.nixfmt-tree;
+      }
+    );
+}
